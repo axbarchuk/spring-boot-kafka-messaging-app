5ae56291c6b4c0bcf8310152032971f46faf289b
Alex Barchuk <ax.barchuk@gmail.com>
2022-06-07 19:02:13 +0300

    Fixed gradle dependecies conflicts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,12 +15,26 @@ repositories {
 	mavenCentral()
 }
 
+extra["springCloudVersion"] = "2021.0.3"
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.apache.kafka:kafka-streams")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.cloud:spring-cloud-stream")
+	implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
+	implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka-streams")
+	implementation("org.springframework.kafka:spring-kafka")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/ax/barchuk/messaging/KafkaMessagingApplication.kt
+++ src/main/kotlin/ax/barchuk/messaging/KafkaMessagingApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringBootKafkaMessagingAppApplication
+class KafkaMessagingApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootKafkaMessagingAppApplication>(*args)
+	runApplication<KafkaMessagingApplication>(*args)
 }
